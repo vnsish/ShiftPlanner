2aa30c46b2566202ca47def5dce525fa4eb462f7
Eric Monné Mesalles <emonne97@gmail.com>
2018-09-16 13:30:23 -0300

    Printable

--- ShiftPlanner.py
+++ ShiftPlanner.py
@@ -1,15 +1,12 @@
 """
 Problema: Alocao de turnos
-
 Individuo:
-
 Matriz 7xN onde N é o número de funcionários a serem alocados
 Valores a serem inseridos:
 0 - Folga
 1 - Turno do dia
 2 - Turno da tarde
 3 - Turno da noite
-
 Exemplo
     | D | S | T | Q | Q | S | S |
 F1  | 1 | 1 | 1 | 0 | 0 | 2 | 2 |
@@ -17,28 +14,19 @@ F2  | 2 | 2 | 2 | 1 | 1 | 0 | 0 |
 F3  | 3 | 3 | 3 | 0 | 0 | 1 | 1 |
 F4  | 0 | 0 | 0 | 3 | 3 | 3 | 3 |
 F5  | 1 | 1 | 1 | 0 | 0 | 1 | 1 |
-
 O alocamento deve ser feito de acordo com uma segunda tabela, que
 determina o número mínimo de funcionário por turno em cada dia da semana
-
 Exemplo tabela de requisitos
         | D | S | T | Q | Q | S | S |
 0(dia)  | 3 | 3 | 2 | 2 | 2 | 3 | 3 |
 1(tarde)| 1 | 1 | 2 | 2 | 2 | 1 | 1 |
 2(noite)| 0 | 1 | 1 | 1 | 1 | 1 | 0 |
-
 Entradas do usuário: número de funcionários e tabela de requisitos
-
 Cálculo do fitness
 Para cada turno:
-
 |Numero de funcionarios necessarios - numero de funcionarios alocados|
-
 O fitness será a somatória desse cálculo para os 3 turnos, quando o fitness
 do individuo é igual a 0, a solução é aceita.
-
-
-
 """
 
 import random
@@ -133,10 +121,8 @@ def main():
     req = [[1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1]]
     dias = ['Domingo', 'Segunda-feira', 'Terca-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sabado']
 
-    for i in range(len(req)):
-        for j in range(len(req[0])):
-            req[i][j] = int(input('Turno {0} - {1}: '.format(i+1, dias[j])))
-
+   
+   
     found = 0
 
     while found == 0:
@@ -157,7 +143,14 @@ def main():
          c.mutar()
          nova.append(c)
       populacao = nova
-    print(solucao.tabela)
+    dias = ("\n\t|Doming\t|Segun\t|Terca\t|Quarta\t|Quinta\t|Sexta\t|Sabado |")
+    print(dias)
+    print("-"*(len(dias)+(2*6))) #2  +- len("/t")
+    id=1
+    for raw in solucao.tabela:
+      print(id,"\t|",raw[0], "\t|", raw[1],"\t|", raw[2],"\t|", raw[3],"\t|", raw[4],"\t|", raw[5],"\t|", raw[6], "\t|")
+      print("-"*(len(dias)+(2*6)))
+      id+=1
 
 
 main()
